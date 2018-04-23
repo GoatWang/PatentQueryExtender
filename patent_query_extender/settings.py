c91652c3a7262d04967c70137b914747f7befd25
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-04-23 23:14:58 +0800

    allow host add

--- patent_query_extender/settings.py
+++ patent_query_extender/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '=5+^z+kr*1vhm81d(z9#j*1lhq9$4&*+w+*dyylfrhpqoez(qt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://patent-query-extender-dev.us-west-2.elasticbeanstalk.com',
+]
 
 
 # Application definition
