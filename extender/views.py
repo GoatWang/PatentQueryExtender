344ee7a2125f46b1c091a5ab096f5b3caa298c12
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-05-14 08:43:02 +0800

    load model in memory

--- extender/views.py
+++ extender/views.py
@@ -5,8 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 import os
-from gensim.models import Word2Vec
-import gensim
+import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -25,37 +24,37 @@ jieba.set_dictionary(os.path.join(settings.MEDIA_ROOT, 'dict.txt.big'))
 #     stopwords = f.read().split('\n')
 
 
-def download_gensim_model():
-    # model_path = os.path.join(settings.MEDIA_ROOT, 'downloaded', 'patent_non_stops_top50.zh.model') 
-    try:
-        model_path = os.path.join(settings.MEDIA_ROOT, 'downloaded', 'patent_non_stops_top191.zh.model') 
-        # model_path = os.path.join(settings.MEDIA_ROOT, 'downloaded', 'patent_non_stops_top100.zh.model') 
-        # model_path = os.path.join(settings.MEDIA_ROOT, 'downloaded', 'patent_non_stops_top50.zh.model') 
-        # model_path = os.path.join(settings.MEDIA_ROOT, 'downloaded', 'patent_non_stops_top20.zh.model') 
-        model = Word2Vec.load(model_path)
-    except:
-        model_path = os.path.join(settings.MEDIA_ROOT, 'patent_non_stops_top20.zh.model') 
-        model = Word2Vec.load(model_path)
-    print("model:", model_path)
-    return model
+from extender.load_model import word2vec_model, title_vectors
 
 
 def retrieve_titles(terms, next):
-    model = download_gensim_model()
+    from datetime import datetime
+    st = datetime.now()
     title_vector = np.zeros((400,))
+
     for t in terms:
-        if t in model.wv.vocab:
-            title_vector += model.wv[t]
+        if t in word2vec_model.wv.vocab:
+            title_vector += word2vec_model.wv[t]
 
     if np.array_equal(title_vector, np.zeros((400,))):
         relevant_titles = []
     else:
-        title_vectors = np.load(os.path.join(settings.MEDIA_ROOT, 'downloaded', 'title_vectors_top191.npy'))
+        # title_vectors = np.load(os.path.join(settings.MEDIA_ROOT, 'downloaded', 'title_vectors_top191.npy'))
         scores = cosine_similarity(np.array(title_vector).reshape(1, -1), title_vectors)[0]
+        print(datetime.now() - st, "scores")
+
         retrieved = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[next:next+50]
+        print(datetime.now() - st, "sorted")
+        
+        df_relevant_titles = pd.DataFrame([rt for rt in db.patent.find({"_id" : {"$in" : retrieved}})])
+        df_relevant_titles['english_title'] = df_relevant_titles['english_title'].fillna('')
+        print(datetime.now() - st, "dbtime")
+
         relevant_titles = []
         for i in retrieved:
-            relevant_titles.append(db.patent.find({'_id':i})[0])
+            relevant_titles.append(df_relevant_titles.loc[df_relevant_titles['_id']==i].iloc[0].astype(str).to_dict())
+        print(datetime.now() - st, "sorted dbtime")
+        
     return relevant_titles
 
 
@@ -65,20 +64,20 @@ def index(request):
 @csrf_exempt
 def search_relevant_terms(request, term="測試"):
     if request.method == "GET":  # deal with only one word
-        model = download_gensim_model()
-        return_terms = dict(model.most_similar(positive=term.strip()))
+
+        return_terms = dict(word2vec_model.most_similar(positive=term.strip()))
         return JsonResponse(return_terms) 
     if request.method == "POST":
         data = request.POST
         lines = data['terms_str'].split('\n')
         related_terms = []
 
-        model = download_gensim_model()
+
         for line in lines:
             terms = line.split(',') 
-            terms_in_vocab = [t for t in terms if t in model.wv.vocab]            
+            terms_in_vocab = [t for t in terms if t in word2vec_model.wv.vocab]            
             if len(terms_in_vocab) != 0:
-                return_terms = list(map(lambda x:x[0].strip(), model.most_similar(positive=terms_in_vocab)))
+                return_terms = list(map(lambda x:x[0].strip(), word2vec_model.most_similar(positive=terms_in_vocab)))
                 related_terms.extend(return_terms)
 
         if len(related_terms) == 0:
@@ -108,9 +107,8 @@ def search_relevant_titles(request, title="", next=0):
 
 
 def visualize(request):
-    model = download_gensim_model()
-    vocabs = sample(list(model.wv.vocab), 300)
-    X = model[vocabs]
+    vocabs = sample(list(word2vec_model.wv.vocab), 300)
+    X = word2vec_model[vocabs]
 
     n_clusters = 4
     cls_array = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(X)
